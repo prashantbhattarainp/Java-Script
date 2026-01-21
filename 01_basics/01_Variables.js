f6f8d54dd57401e3ef0b812c2cd9893398a3c2bb
Prashant Bhattarai <techandcode.prashant@gmail.com>
2026-01-21 05:57:08 +0000

    Datatypes and Variables

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,18 @@
+const accountID = 123456
+let accountEmail = "prashant.google.com"
+var accountPassword = "456123"
+accountCity = "Pokhara"
+
+let accountState;
+accountEmail ="hc@hc.com"
+accountPassword="1234561"
+acccountCity ="Kathmandu"
+
+
+/*
+Prefer not to use var 
+Because of Issue in block scope and funxtional scope
+*/
+//accountID = 2345
+console.log(accountID)
+console.table([accountID, accountEmail, accountPassword, acccountCity, accountState])
---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS Code as newer version
+
+// alert(3+3) //We are using nodejs, not browser
+
+// number => 2 ^53
+// bigint =>
+// string =>""
+// boolean => true/false
+// null => standalone value  
+// undefined => 
+// symbol => unique
+
+// object 
+
+console.log(typeof("Pacific"))
+console.log(typeof(undefined));
+console.log(typeof null);
+
+
