9a86da02bf907ccd94affdc31c61b2e7fc76302d
Prashant Bhattarai <techandcode.prashant@gmail.com>
2026-01-21 06:16:01 +0000

    Type Conversion

---
+++ 01_basics/03_ConversionsAndOperations.js
@@ -0,0 +1,24 @@
+//let score =true
+// console.log(typeof score)
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN Not a number
+// true => 1 ; false => 0;
+
+// let isLoggedIn = "Prashant"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1=> true; 0=> false
+// "" => false
+// "Prashant" => true
+
+let someNum = 33
+let StringNumb = String(someNum)
+console.log(StringNumb)
+console.log(typeof StringNumb)
